04e517166497751ce8279e06aa06d257a7f2ec46
geraldofieg <geraldofieg@gmail.com>
2026-02-21 16:53:19 -0300

    Add Firebase initialization and configuration

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB29PzKLW5YG_8qIib-swms4veFI4Ev-F4",
+  authDomain: "feedbacks-mais-medicos-novo.firebaseapp.com",
+  projectId: "feedbacks-mais-medicos-novo",
+  storageBucket: "feedbacks-mais-medicos-novo.firebasestorage.app",
+  messagingSenderId: "390908095939",
+  appId: "1:390908095939:web:f8cd055c90c8d26e666176"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
