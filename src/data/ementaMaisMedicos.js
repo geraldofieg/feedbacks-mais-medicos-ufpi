80385f694036aa72b694aaa2dd7888b292159222
geraldofieg <geraldofieg@gmail.com>
2026-03-12 14:40:02 -0300

    Create ementaMaisMedicos.js

---
+++ src/data/ementaMaisMedicos.js
@@ -0,0 +1,71 @@
+// src/data/ementaMaisMedicos.js
+export const ementaMaisMedicos = [
+  { 
+    eixo: "Eixo 1 - Princípios e Fundamentos do SUS e da APS", 
+    modulos: [
+      { num: "01", titulo: "Políticas públicas de saúde: processo histórico e a organização do SUS", ch: 15, creditos: 1, semanas: 2 },
+      { num: "02", titulo: "Atenção Primária à Saúde e Estratégia Saúde da Família", ch: 15, creditos: 1, semanas: 2 },
+      { num: "03", titulo: "Princípios da Medicina de Família e Comunidade", ch: 15, creditos: 1, semanas: 2 },
+    ]
+  },
+  { 
+    eixo: "Eixo 2 - Ferramentas da Medicina de Família e Comunidade", 
+    modulos: [
+      { num: "04", titulo: "Ferramentas de abordagem clínica", ch: 30, creditos: 2, semanas: 4 },
+      { num: "05", titulo: "Gestão da clínica e coordenação do cuidado", ch: 30, creditos: 2, semanas: 4 },
+      { num: "06", titulo: "Abordagem familiar", ch: 30, creditos: 2, semanas: 4 },
+      { num: "07", titulo: "Abordagem comunitária", ch: 30, creditos: 2, semanas: 4 },
+    ]
+  },
+  { 
+    eixo: "Eixo 3 - Cuidado a grupos específicos e situações especiais", 
+    modulos: [
+      { num: "08", titulo: "Saúde da criança e do adolescente", ch: 30, creditos: 2, semanas: 4 },
+      { num: "09", titulo: "Saúde da mulher", ch: 30, creditos: 2, semanas: 4 },
+      { num: "10", titulo: "Saúde do homem", ch: 15, creditos: 1, semanas: 2 },
+      { num: "11", titulo: "Saúde do idoso", ch: 15, creditos: 1, semanas: 2 },
+    ]
+  },
+  { 
+    eixo: "Eixo Transversal", 
+    modulos: [
+      { num: "34", titulo: "Metodologia científica (TCC)", ch: 45, creditos: 3, semanas: 6 }
+    ]
+  },
+  { 
+    eixo: "Eixo 4 - Atenção à Saúde", 
+    modulos: [
+      { num: "12", titulo: "Abordagem a problemas gerais e inespecíficos", ch: 30, creditos: 2, semanas: 4 },
+      { num: "13", titulo: "Abordagem a Problemas Respiratórios, nariz, ouvido e garganta", ch: 30, creditos: 2, semanas: 4 },
+      { num: "14", titulo: "Abordagem a Problemas de Saúde Mental", ch: 30, creditos: 2, semanas: 4 },
+      { num: "15", titulo: "Abordagem a Problemas Digestivos", ch: 15, creditos: 1, semanas: 2 },
+      { num: "16", titulo: "Abordagem a Problemas Infecciosos", ch: 30, creditos: 2, semanas: 4 },
+      { num: "17", titulo: "Abordagem a Problemas Cardiovasculares", ch: 30, creditos: 2, semanas: 4 },
+      { num: "18", titulo: "Abordagem a Problemas Dermatológicos", ch: 15, creditos: 1, semanas: 2 },
+      { num: "19", titulo: "Abordagem a Problemas Hematológicos", ch: 15, creditos: 1, semanas: 2 },
+      { num: "20", titulo: "Abordagem a Problemas Metabólicos", ch: 30, creditos: 2, semanas: 4 },
+      { num: "21", titulo: "Abordagem a Problemas de Vias Urinárias", ch: 15, creditos: 1, semanas: 2 },
+      { num: "22", titulo: "Abordagem a Problemas Musculoesqueléticos", ch: 30, creditos: 2, semanas: 4 },
+      { num: "23", titulo: "Abordagem a Problemas do Sistema Nervoso", ch: 30, creditos: 2, semanas: 4 },
+      { num: "24", titulo: "Abordagem a Problemas de Olhos e Visão", ch: 15, creditos: 1, semanas: 2 },
+    ]
+  },
+  { 
+    eixo: "Eixo 5 - Grupos Específicos e Situações Especiais (Cont.)", 
+    modulos: [
+      { num: "25", titulo: "Atenção à saúde das populações do campo, florestas e águas", ch: 15, creditos: 1, semanas: 2 },
+      { num: "26", titulo: "Atenção às situações de violência", ch: 15, creditos: 1, semanas: 2 },
+      { num: "27", titulo: "Atenção à saúde do trabalhador", ch: 15, creditos: 1, semanas: 2 },
+      { num: "28", titulo: "Atenção à sexualidade", ch: 15, creditos: 1, semanas: 2 },
+    ]
+  },
+  { 
+    eixo: "Eixo 6 - Procedimentos e Organizações Específicas do Cuidado", 
+    modulos: [
+      { num: "29", titulo: "Realização de Procedimentos na APS", ch: 30, creditos: 2, semanas: 4 },
+      { num: "30", titulo: "Urgências e Emergências na APS", ch: 30, creditos: 2, semanas: 4 },
+      { num: "31", titulo: "Cuidados domiciliares", ch: 15, creditos: 1, semanas: 2 },
+      { num: "32", titulo: "Cuidados paliativos", ch: 15, creditos: 1, semanas: 2 },
+    ]
+  }
+];
